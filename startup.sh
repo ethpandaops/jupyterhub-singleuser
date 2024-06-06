8336fef4977823f085a18614767e29fa6dd9580b
Andrew Davis <1709934+Savid@users.noreply.github.com>
2024-06-06 15:17:26 +1000

    feat: add xatu-data examples

--- startup.sh
+++ startup.sh
@@ -6,7 +6,9 @@ GITHUB_REPO_DIR=/tmp/xatu-data
 git clone $GITHUB_REPO_URL $GITHUB_REPO_DIR
 
 rm -rf $HOME/examples
+mkdir -p $HOME/examples
 cp -r $GITHUB_REPO_DIR/examples/clickhouse/* $HOME/examples/
+rm -rf $GITHUB_REPO_DIR
 
 # Start Jupyter Lab
 exec jupyter lab
