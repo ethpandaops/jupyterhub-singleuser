fa897f5eb1a4d60f98efa799e4fc800c298b89d9
Andrew Davis <1709934+Savid@users.noreply.github.com>
2024-06-06 15:42:48 +1000

    fix home path on startup

--- startup.sh
+++ startup.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
 # Clone xatu examples
-GITHUB_REPO_URL=https://github.com/ethpandaops/xatu-data.git
-GITHUB_REPO_DIR=/tmp/xatu-data
-git clone $GITHUB_REPO_URL $GITHUB_REPO_DIR
-
+git clone https://github.com/ethpandaops/xatu-data.git /tmp/xatu-data
 rm -rf /home/jovyan/examples
 mkdir -p /home/jovyan/examples
-cp -r $GITHUB_REPO_DIR/examples/clickhouse/* /home/jovyan/examples/
-rm -rf $GITHUB_REPO_DIR
+cp -r /tmp/xatu-data/examples/clickhouse/* /home/jovyan/examples/
+rm -rf /tmp/xatu-data
 
 # Start Jupyter Lab
 exec jupyter lab
